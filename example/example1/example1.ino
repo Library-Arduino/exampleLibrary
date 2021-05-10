264e0e26765b7970b5e98f1cdbd672da995a5f6e
Jairo Rohatan <64938211+Jairo2020@users.noreply.github.com>
2021-05-10 15:06:37 -0500

    updated

--- example/example1/example1.ino
+++ example/example1/example1.ino
@@ -9,13 +9,13 @@ void setup()
 
 void loop()
 {
-    Serial.println(example.suma(1, 3));
-    Serial.println(example.resta(6, 4));
-    Serial.println(example.multiplicar(3, 2));
-    Serial.println(example.variablePublica);
-    Serial.println(example.mostrarValorPrivado());
-    Serial.println(example.mostrarPi(3.141592), 6);
-    Serial.println(PI, 6);
+    Serial.println("Suma = " + String(example.suma(1, 3)));
+    Serial.println("Resta = " + String(example.resta(6, 4)));
+    Serial.println("Multiplicación = " + String(example.multiplicar(3, 2)));
+    Serial.println("Variable publica = " + example.variablePublica);
+    Serial.println("Variable privada = " + String(example.mostrarValorPrivado()));
+    Serial.println(" Valor de Pi por parámetro = " + String(example.mostrarPi(3.141592), 6));
+    Serial.println("Valor de PI = " + String(PI, 6));
 
     delay(1000);
 }
